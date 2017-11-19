65e2fe1f7ccee7ffec882aea282e894d68649d9a
Mistty <shenhaichen@outlook.com>
2017-11-19 23:41:33 +1100

    practice

--- Solution.java
+++ Solution.java
@@ -1,34 +1,12 @@
-import java.io.*;
+//Complete this code or write your own from scratch
 import java.util.*;
+import java.io.*;
 
-public class Solution {
-
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+class Solution{
+    public static void main(String []argh) {
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        List<Integer> list = new ArrayList<Integer>();
-        for(int i = 0; i<N; i++){
-            list.add(i, in.nextInt());
-        }
-        List<Integer> listQ = new ArrayList<Integer>();
-        int Q = in.nextInt();
-        for(int i =0; i<Q; i++){
-            String qry = in.next();
-
-            if(qry.startsWith(("Insert"))){
-                for(int u = 0; u<2; u++){
-                    listQ.add(u, in.nextInt());
-                }
-                list.add(listQ.get(0), listQ.get(1));
-            }else if(qry.startsWith("Delete")){
-                list.remove(in.nextInt());
-            }
-        }
-
-        for(int i =0; i<list.size(); i++){
-            System.out.print(list.get(i)+" ");
-        }
+        int n=in.nextInt();
+        in.nextLine();
 
     }
 }
