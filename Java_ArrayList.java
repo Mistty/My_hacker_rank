9c77723599d40c152829fb41b68b231a126fca3a
Mistty <shenhaichen@outlook.com>
2017-11-19 17:49:14 +1100

    practice

--- Java_ArrayList.java
+++ Java_ArrayList.java
@@ -7,7 +7,7 @@ public class Solution {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        
+
         ArrayList[] a = new ArrayList[n];
         for(int i=0; i<n; i++){
             int d = in.nextInt();
@@ -16,7 +16,7 @@ public class Solution {
                 a[i].add(in.nextInt());
             }
         }
-        
+
         int q = in.nextInt();
         for(int i=0; i<q; i++){
             int h = in.nextInt();
