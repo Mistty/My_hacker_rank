512aa1fbb270d5b5be66a4c96bba092704519061
Mistty <shenhaichen@outlook.com>
2017-11-20 22:42:54 +1100

    practice

--- Solution.java
+++ Solution.java
@@ -1,25 +1,10 @@
 import java.util.*;
-import java.io.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
-class Solution {
-    public static void main(String[] argh) {
-        Scanner s = new Scanner(System.in);
-        int t = s.nextInt();
-        String[] pair_left = new String[t];
-        String[] pair_right = new String[t];
+public class Solution {
 
-        for (int i = 0; i < t; i++) {
-            pair_left[i] = s.next();
-            pair_right[i] = s.next();
-        }
+    public static void main(String args[]) {
 
-        Set<String> hset = new HashSet<String>();
-        for(int i = 0; i<t; i++){
-            hset.add(pair_left[i]+" "+pair_right[i]);
-            System.out.println(hset.size());
-        }
+
+
     }
 }
