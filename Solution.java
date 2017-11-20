9c761e1322ed815962b00df551ebf4d4cf2574b2
Mistty <shenhaichen@outlook.com>
2017-11-20 21:16:35 +1100

    practice

--- Solution.java
+++ Solution.java
@@ -1,12 +1,10 @@
-//Complete this code or write your own from scratch
 import java.util.*;
 import java.io.*;
 
 class Solution{
-    public static void main(String []argh) {
+    public static void main(String []argh){
         Scanner in = new Scanner(System.in);
-        int n=in.nextInt();
-        in.nextLine();
 
+        }
     }
 }
