abbc514b1394c410d3c6a3505208288b1cefb928
Mistty <shenhaichen@outlook.com>
2017-11-21 21:40:16 +1100

    practice

--- Solution.java
+++ Solution.java
@@ -1,29 +1,28 @@
-import java.io.*;
 import java.util.*;
+
 public class Solution {
+    //public static boolean canWin(int n,int leap, int[] game) {
+    private static boolean canWin(int m, int[] game, int n) {
+        if (n < 0 || game[n] == 1) return false;
+        if ((n == game.length - 1) || n + m > game.length - 1) return true;
+
+        game[n] = 1;
+        return canWin(m, game, n + 1) || canWin(m, game, n - 1) || canWin(m, game, n + m);
+    }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        if(scan.hasNext()){
-            String s = scan.nextLine();
+        int q = scan.nextInt();
+        while (q-- > 0) {
+            int n = scan.nextInt();
+            int leap = scan.nextInt();
 
-            String[] items = s.trim().split("[ !,?.\\_'@]+");
-
-            if(s == ""){
-                System.out.println("0");
-            }
-            else if(s.length() > 400000){
-                return;
-            }
-            else{
-               System.out.println(items.length);
+            int[] game = new int[n+leap];
+            for (int i = 0; i < n; i++) {
+                game[i] = scan.nextInt();
             }
 
-            for(int i =0; i< items.length; i++){
-                System.out.println(items[i]);
-            }
-        }else{
-            System.out.println("0");
+            System.out.println( (canWin(leap, game, 0)) ? "YES" : "NO" );
         }
         scan.close();
     }
