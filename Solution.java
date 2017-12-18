a8d078a6e3a70a77ff29e0b15c1896bade607915
Mistty <shenhaichen@outlook.com>
2017-12-18 17:02:52 +0800

    tes

--- Solution.java
+++ Solution.java
@@ -1,29 +1,23 @@
+import java.io.*;
 import java.util.*;
 
 public class Solution {
-    //public static boolean canWin(int n,int leap, int[] game) {
-    private static boolean canWin(int m, int[] game, int n) {
-        if (n < 0 || game[n] == 1) return false;
-        if ((n == game.length - 1) || n + m > game.length - 1) return true;
-
-        game[n] = 1;
-        return canWin(m, game, n + 1) || canWin(m, game, n - 1) || canWin(m, game, n + m);
-    }
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int q = scan.nextInt();
-        while (q-- > 0) {
-            int n = scan.nextInt();
-            int leap = scan.nextInt();
-
-            int[] game = new int[n+leap];
-            for (int i = 0; i < n; i++) {
-                game[i] = scan.nextInt();
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        //List<Integer> row[] = new ArrayList<>();
+        //List<Integer> arr = new ArrayList<>();
+        int[][] arr = new int[6][6];
+        for(int n = 0; n<6; n++){
+            for(int m = 0; m<6; m++){
+                arr[n][m] = in.nextInt();
             }
-
-            System.out.println( (canWin(leap, game, 0)) ? "YES" : "NO" );
         }
-        scan.close();
+        System.out.print(arr);
+
+        //System.out.print(arr);
+
+
     }
 }
