90ba75b5cbb83992eb19a78f2270c82581061a40
Mistty <shenhaichen@outlook.com>
2017-12-20 00:20:51 +0800

    tes

--- Dynamic_Array.java
+++ Dynamic_Array.java
@@ -1,7 +1,8 @@
-import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class Solution {
+public class Dynamic_Array {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
---
+++ TimeGlassSum.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class TimeGlassSum {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        //List<Integer> row[] = new ArrayList<>();
+        //List<Integer> arr = new ArrayList<>();
+        int[][] arr = new int[6][6];
+        for(int n = 0; n<6; n++){
+            for(int m = 0; m<6; m++){
+                arr[n][m] = in.nextInt();
+            }
+        }
+        //System.out.print(arr);
+        int re = -999999999;
+        for(int i =0; i<4; i++){
+            for(int u = 0; u<4; u++){
+                int sum = arr[i][u] + arr[i][u+1] + arr[i][u+2] + arr[i+1][u+1] + arr[i+2][u] + arr[i+2][u+1] + arr[i+2][u+2];
+                re = sum>re?sum:re;
+            }
+        }
+        System.out.println(re);
+
+    }
+}
